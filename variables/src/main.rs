97b2168d0d8f15b1624efa42e46ae8be591fda6b
codexnii0 <caynn03@gmail.com>
2024-12-26 00:55:51 +0800

    update

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,4 +1,19 @@
 fn main() {
-    let x = 4;
+    let mut x = 4;
     println!("x is {}", x);
+
+    //name shadowing
+    {
+        let x = 2;
+        println!("x is {}", x);
+    }
+
+    //now make the first x mutable so it will work
+    x = 5;
+    println!("x is {}", x);
+
+    //CONSTANTS (must be all caps)
+    const SECONDS_IN_MINUTE: u32 = 60;
+    println!("{}", SECONDS_IN_MINUTE);
+    //constants cannot change value unlike let
 }
