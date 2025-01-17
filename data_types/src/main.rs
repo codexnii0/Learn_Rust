0e2f3708d1388081dec78d03c5c7059d91ee9204
codexnii0 <caynn03@gmail.com>
2025-01-17 14:01:01 +0800

    update

---
+++ data_types/src/main.rs
@@ -0,0 +1,5 @@
+fn main() {
+    let x: i32 = 100;
+    println!("x is {}", x);
+    //i32 is integer [default]
+}
---
+++ data_types/target/debug/data_types.d
@@ -0,0 +1,1 @@
+/home/codexnii/Downloads/Learn_Rust/data_types/target/debug/data_types: /home/codexnii/Downloads/Learn_Rust/data_types/src/main.rs
